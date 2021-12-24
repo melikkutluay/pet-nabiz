35766c7159f777d031a7c0eaa7986e2e74e4ee8a
Hilal Erisev <hilalerisev@Hilal-Air.home>
2021-12-25 00:05:45 +0300

    commit

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("PET NABIZ'A HOŞ GELDİN !")
+  res.send("SALAKLAR BİR ŞEYİ BECEREMEDİNİZ!")
 })
 
 app.listen(process.env.PORT || 3000, function(){
