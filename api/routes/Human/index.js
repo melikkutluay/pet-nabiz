23d32ef756cbf96d83961ab5bfa8b69395a56465
melikkutluay <milliler04@gmail.com>
2021-12-25 20:10:19 +0300

    .

--- api/routes/Human/index.js
+++ api/routes/Human/index.js
@@ -3,10 +3,16 @@ const router = express()
 const db = require('../../queryBuild')
 const queryBuilder = new db.exec('Human')
 
-router.get('/', async (req, res, next) => {
+router.get('/:humanId?', async (req, res, next) => {
     try {
-        let response = await queryBuilder.select('*');
-        res.status(200).json(response)
+        if (req.params.humanId) {
+            console.log("9 line :",req.body);
+            let response = await queryBuilder.select('*').where('Mail', req.params.humanId && 'Password', req.body.Password);
+            res.status(200).json(response)
+        } else {
+            let response = await queryBuilder.select('*');
+            res.status(200).json(response)
+        }
     } catch (error) {
         next(error)
     }
