dc91f84ed48d508ddaa5c02480483e6de077ed6e
melikkutluay <milliler04@gmail.com>
2021-12-25 19:19:44 +0300

    .

--- api/routes/Pet/index.js
+++ api/routes/Pet/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const router = express()
+const db = require('../../queryBuild')
+const queryBuilder = new db.exec('Pets')
 
+router.get('/', async (req, res, next) => {
+    try {
+        let response = await queryBuilder.select('*');
+        res.status(200).json(response)
+    } catch (error) {
+        next(error)
+    }
+})
 
 module.exports = router;
