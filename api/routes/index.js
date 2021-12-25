ba7ad8628f6b7838a6d3b5f94c355b0153e196bd
melikkutluay <milliler04@gmail.com>
2021-12-25 19:27:50 +0300

    .

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express()
 
+router.use('/', require('./Main'))
 router.use('/Human', require('./Human'))
 router.use('/Process', require('./Process'))
 router.use('/Pet', require('./Pet'))
