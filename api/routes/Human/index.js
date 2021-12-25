160fd530e5f5e5dbd7baf008c17e2d2feffc0bd4
melikkutluay <milliler04@gmail.com>
2021-12-25 21:22:28 +0300

    .

--- api/routes/Human/index.js
+++ api/routes/Human/index.js
@@ -2,14 +2,13 @@ const express = require('express')
 const router = express()
 const _ = require('lodash')
 const db = require('../../queryBuild')
-const queryBuilder = new db.exec('Human')
 
 router.post('/:humanId?', async (req, res, next) => {
     try {
         console.log(req.body);
         if (req.params.humanId) {
+            const queryBuilder = new db.exec('Human')
             let response = await queryBuilder.select('*').where('Mail', req.body.Mail).where('Password', req.body.Password);
-            console.log("response.lenght:",_.isEmpty(response));
             if (_.isEmpty(response)) {
                 res.status(200).json(400)
             }else {
