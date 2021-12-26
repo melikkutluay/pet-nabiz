eb11bb989c4b14787ad488888aea6de10205bb52
melikkutluay <milliler04@gmail.com>
2021-12-26 09:28:29 +0300

    .

--- api/apicall.js
+++ api/apicall.js
@@ -8,8 +8,13 @@ module.exports = {
                 return await queryBuilder.select('*');
             } else {
                 if (_.has(data, 'process_date')) {
+                    let {
+                        first_time,
+                        second_time
+                    } = data.process_date
                     const rawQuery = db.queryClass
-                    return rawQuery.schema.raw("select p1.process, p1.process_type, p1.process_date, p2.Name from process p1, pet p2 where p1.pet_id = p2.id and substr(process_date,7)||substr(process_date,4,2)||substr(process_date,1,2) between '20211201' and '20211231'");
+                    return rawQuery.schema.raw(`select p1.process, p1.process_type, p1.process_date, p2.Name from process p1, pet p2 where p1.pet_id = p2.id and substr(process_date,7)||substr(process_date,4,2)||substr(process_date,1,2) between substr('${first_time}',7)||substr('${first_time}',4,2)||substr('${first_time}',1,2) and substr('${second_time}',7)||substr('${second_time}',4,2)||substr('${second_time}',1,2)`);
+                    
                 } else {
                     const queryBuilder = new db.exec(tableName)
                     return await queryBuilder.select('*').where(data);
