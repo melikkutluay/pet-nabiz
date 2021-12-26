b97cd304cbdaff61ea8ad16421a798479207da59
melikkutluay <milliler04@gmail.com>
2021-12-26 10:17:15 +0300

    .

--- api/routes/Main/index.js
+++ api/routes/Main/index.js
@@ -5,9 +5,14 @@ const _ = require('lodash')
 
 router.get('/:humanId?', async (req, res, next) => {
     try {
+        let resArr = [];
         if (req.params.humanId) {
             let response = await apicall.get('pet', { Human_id: req.params.humanId });
-            res.status(200).json(response)
+            response.forEach(element => {
+                let { id, Name, Gender, Genus, Age } = element
+                resArr.push({ id, Name, Gender, Genus, Age })
+            });
+            res.status(200).json(resArr)
         }
     } catch (error) {
         next(error)
